78b962213f4a36743da6988b5514a086a873ab03
Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-05-06 13:13:26 +0300

    Fixed CORS

--- src/app/auction/auction.component.ts
+++ src/app/auction/auction.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-auction',
@@ -8,53 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuctionComponent implements OnInit {
 
-  constructor(private http:HttpClient) {
-    this.getData().subscribe(data => {
-      console.warn(data);
-    });
-   }
+  auctions:any = [];
 
-  auctions:any=[];
-
-
-  modalTitle = "";
-  EmployeeId = 0;
-  EmployeeName = "";
-  Department = "";
-  DateOfJoining = "";
-
+  constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
-    this.test()
+    this.getAuctions();
   }
 
-  getData() {
-    let url="https://localhost:5001/api/Auction/get-all-auctions";
-    return this.http.get(url);
-  }
-  refreshList() {
-    this.http.get<any>('https://localhost:5001/api/Auction/get-all-auctions').subscribe(data => {
-      this.auctions=data;
+  getAuctions() {
+    this.http.get(environment.AUCTION_API + 'get-all-auctions').subscribe(response => {
+      this.auctions = response;
     });
   }
-  test() {
-    console.log(this.auctions);
-  }
-
-  editClick(emp:any){
-    this.modalTitle = "Edit Employee";
-    this.EmployeeId = emp.EmployeeId;
-    this.EmployeeName = emp.EmployeeName;
-    this.Department = emp.Department;
-    this.DateOfJoining = emp.DateOfJoining;
-  }
-
-  deleteClick(id:any){
-    if(confirm('Are you sure?')){
-      this.http.delete('https://localhost:5001/api/Auction/get-all-auctions/'+id).subscribe(res => {
-        alert(res.toString());
-        this.refreshList();
-      });
-    }
-  }
+  
 }
