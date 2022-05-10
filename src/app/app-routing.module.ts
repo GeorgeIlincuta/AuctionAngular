50a74892dd932a259e09f7e41945a5967914207a
George Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-05-10 13:38:56 +0300

    made login, register, lost pages work togheter and organized the files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuctionComponent } from './auction/auction.component';
-import { LoginComponent } from './login/login.component';
+import { ForgotComponent } from './form/forgot/forgot.component';
+import { LoginComponent } from './form/login/login.component';
+import { RegisterComponent } from './form/register/register.component';
 
 const routes: Routes = [
   { path:'', component:LoginComponent},
-  { path:'auctions', component:AuctionComponent}
+  { path:'auctions', component:AuctionComponent},
+  { path:'login', component:LoginComponent},
+  { path:'register', component:RegisterComponent},
+  { path:'forgot', component:ForgotComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuctionComponent } from './auction/auction.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './form/login/login.component';
+import { RegisterComponent } from './form/register/register.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AuctionCardComponent } from './auction-card/auction-card.component';
+import { ForgotComponent } from './form/forgot/forgot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuctionComponent,
     LoginComponent,
-    AuctionCardComponent
+    AuctionCardComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
