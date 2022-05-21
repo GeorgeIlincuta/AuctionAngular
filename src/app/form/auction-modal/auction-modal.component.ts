72f44aceb1d4123ad91989bd59aca250116d7115
George Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-05-21 18:04:25 +0300

    added first part of auction front to backend create

--- src/app/form/auction-modal/auction-modal.component.ts
+++ src/app/form/auction-modal/auction-modal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormControl,} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuctionService } from 'src/app/service/auction.service';
 
 @Component({
   selector: 'auction-modal',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuctionModalComponent implements OnInit {
 
-  constructor() { }
+  public addAuction = this.formBuilder.group({
+    title:['',Validators.required],
+    description:['',Validators.required],
+    bidStartPrice:['',Validators.required],
+    bidDuration:['',Validators.required],
+    bidBuyingPrice:['',Validators.required],
+  })
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private auctionService: AuctionService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    let name = this.addAuction.controls["title"].value;
+    let description = this.addAuction.controls["description"].value;
+    let bidStart = parseInt(this.addAuction.controls["bidStartPrice"].value);
+    let timeLeft = parseInt(this.addAuction.controls["bidDuration"].value);
+    let buyPrice = parseInt(this.addAuction.controls["bidBuyingPrice"].value);
+    let active = true;
+    let lastBid = null;
+    let appUserId = 2;
+    let appUser = null
+    this.addAuction.reset();
+
+    this.auctionService.addAuction(name, description, bidStart, timeLeft, buyPrice, active, lastBid, appUserId, appUser).subscribe((data:any) => {
+      console.log("response", data);
+    }, error => {
+      console.log("error", error)
+    })
+    window.location.reload()
+  }
+
+  get Title(): FormControl {
+    return this.addAuction.get('title') as FormControl;
+  }
+  get Description(): FormControl {
+    return this.addAuction.get('description') as FormControl;
+  }
+  get BidStartPrice(): FormControl {
+    return this.addAuction.get('bidStartPrice') as FormControl;
+  }
+  get BidDuration(): FormControl {
+    return this.addAuction.get('bidDuration') as FormControl;
+  }
+  get BidBuyingPrice(): FormControl {
+    return this.addAuction.get('bidBuyingPrice') as FormControl;
   }
 
 }
--- src/app/service/auction.service.ts
+++ src/app/service/auction.service.ts
@@ -4,6 +4,12 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Auction } from '../models/auction.component';
 
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('jwt')!).token
+//   })
+// }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +19,22 @@ export class AuctionService {
 
   // Get all auctions
   getAllAuctions(): Observable<Auction[]> {
-
     return this.http.get<Auction[]>(environment.AUCTION_API + 'get-all-auctions');
+  }
+
+  addAuction(name:string, description:string, bidStartPrice:number, bidDuration:number, bidBuyingPrice:number,
+    active:boolean, lastBid:null, appUserId:number, appUser:null) {  
+    const body = {
+      name:name,
+      description:description,
+      bidStart:bidStartPrice,
+      timeLeft:bidDuration,
+      buyPrice:bidBuyingPrice,
+      active:active,
+      lastBid:lastBid,
+      appUserId:appUserId,
+      appUser:appUser
+    }
+    return this.http.post(environment.AUCTION_API + 'add-auction', body)
   }
 }
